5079df272d414fdbde75925393cb205c7f553408
Windows <gersoncalienes@gmail.com>
2018-02-23 22:11:28 -0500

    completed first action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,11 @@
 export function selectBook(book) {
-  console.log('A book has been selected:', book.title);
+  // console.log('A book has been selected:', book.title);
+  // selectBook is an ActionCreators, it needs to return an action,
+  //an object with a type property.
+  //An action always contains a type and sometimes (most of the times)
+  //contains a payload.
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
