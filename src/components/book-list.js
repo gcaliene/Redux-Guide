c7fa4241c1b11daf782d348877602b3acfbd7373
Windows <gersoncalienes@gmail.com>
2018-02-23 19:51:40 -0500

    Started BookList

---
+++ src/components/book-list.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+//A container is just a component that has access to the state via Redux
+export default class BookList extends Component {
+  render() {
+    return <ul className="list-group col-sm-4">li</ul>;
+  }
+}
---
+++ src/reducers/books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Harry potter' },
+    { title: 'Dark tower' },
+    { title: 'Rudy' }
+  ];
+}
