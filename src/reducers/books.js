25a305883480ccafadd6c1de5bcb06af285b6b41
Windows <gersoncalienes@gmail.com>
2018-02-24 14:22:36 -0500

    Finished with the redux refresher
    uses bindActionCreators

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry potter' },
-    { title: 'Dark tower' },
-    { title: 'Rudy' }
+    { title: 'Javascript: The Good Parts', pages: 150 },
+    { title: 'Harry potter', pages: 20430 },
+    { title: 'Dark tower', pages: 5000 },
+    { title: 'Rudy', pages: 1 }
   ];
 }
