d8170960e47368339ad9e26e1d74b3b5ed9a7544
Windows <gersoncalienes@gmail.com>
2018-02-23 20:21:17 -0500

    Connected booklist to redux

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 //A container is just a component that has access to the state via Redux
-export default class BookList extends Component {
+class BookList extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li key={book.title} className="list-group-item">
+          {book.title}
+        </li>
+      );
+    });
+  }
+
   render() {
-    return <ul className="list-group col-sm-4">li</ul>;
+    return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
   }
 }
+
+function mapStateToProps(state) {
+  //Whatever is return will shop up as props inside of BookList
+  //We are returning an object in this
+  return {
+    books: state.books
+  };
+}
+
+export default connect(mapStateToProps)(BookList);
