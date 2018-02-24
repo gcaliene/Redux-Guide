f310a0568ef4c33d013dc95691aa02fcd39a982d
Windows <gersoncalienes@gmail.com>
2018-02-23 23:09:16 -0500

    Added BookDetail container to the app component

---
+++ src/reducers/active_book.js
@@ -0,0 +1,10 @@
+//all reducers get 2 arguments, the current state and action
+
+export default function(state = null, action) {
+  switch (action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+
+  return state;
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './books.js';
+import ActiveBook from './active_book.js';
 
 const rootReducer = combineReducers({
   // state: (state = {}) => state
-  books: BooksReducer
+  books: BooksReducer,
+  //remember that any key in combineReducers is a key in the global state
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
